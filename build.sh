1229400e1606e8d7c5c7bb414dfb25ca376c9d50
odelay <odelay941@gmail.com>
2011-09-26 21:45:29 +0900

    Add: build scripts

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Very simple build script
+
+
+
+make ARCH=arm CROSS_COMPILE=$CCOMPILER clean
+make ARCH=arm CROSS_COMPILE=$CCOMPILER defconfig bravo_defconfig
+make ARCH=arm CROSS_COMPILE=$CCOMPILER menuconfig
+make ARCH=arm CROSS_COMPILE=$CCOMPILER -j2
+ZipBuilder/builder.sh
+cp ZipBuilder/update_signed.zip ~/Dropbox/2.6.35-odelay_bravo-kernel.zip
+
+make ARCH=arm CROSS_COMPILE=$CCOMPILER clean
+
+
+
