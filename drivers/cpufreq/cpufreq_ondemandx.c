5fb3a0cf82ef1d498e5b322c76e53ee2e16fca34
odelay <odelay941@gmail.com>
2011-09-27 22:51:35 +0900

    Fix : unmerged files

--- drivers/cpufreq/cpufreq_ondemandx.c
+++ drivers/cpufreq/cpufreq_ondemandx.c
@@ -802,7 +802,4 @@ fs_initcall(cpufreq_gov_dbs_init);
 module_init(cpufreq_gov_dbs_init);
 #endif
 module_exit(cpufreq_gov_dbs_exit);
-<<<<<<< HEAD
 
-=======
->>>>>>> 60e43fb... ADD: "old" Ondemand governor that supports sysfs
